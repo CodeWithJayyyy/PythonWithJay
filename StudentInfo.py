e11d4a5fad573734b850e34d5e923b1f067c3a23
CodeWithJayyyy <Louiejaycinco03@gmail.com>
2026-08-12 19:39:58 +0800

    Add files via upload

---
+++ StudentInfo.py
@@ -0,0 +1,25 @@
+#Student Information Form
+
+student_name = str(input("Enter your name: "))
+student_age = int(input("Enter your age: "))
+student_course = str(input("Enter your course: "))
+student_year = str(input("Enter your year level: "))
+#Survey part 1
+print()
+student_survey = str(input("""What is your favorite subject in your course?
+Enter your favorite subject:  """))
+
+#Student Information:
+
+print("--- Student Information ---")
+print(f"Name: {student_name}")
+print(f"Age: {student_age}")
+print(f"Course: {student_course}")
+print(f"Year: {student_year} year")
+print()
+print("--- Student survey Form: ---")
+#Firt survey or Survey part 1 receipt
+print(f"Student Favorite Subject: {student_survey}")
+
+
+
